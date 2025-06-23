00a5b44817b7e487c99e08ceeacab25a713e80a5
Ben Deane <ben@elbeno.com>
2025-06-22 19:06:57 -0600

    wip

--- test/jeff.cpp
+++ test/jeff.cpp
@@ -16,6 +16,7 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include <barrier>
+#include <chrono>
 #include <deque>
 #include <iostream>
 #include <mutex>
@@ -130,6 +131,7 @@ TEST_CASE("jeff", "[jeff]") {
 }
 
 // super simple thread-safe queue
+// max one element!
 struct msg_queue {
     std::deque<int> q{};
     std::mutex m{};
@@ -137,6 +139,7 @@ struct msg_queue {
     auto push(int x) {
         auto l = std::lock_guard{m};
         q.push_back(x);
+        CHECK(q.size() <= 2);
     }
 
     auto try_pop() -> std::optional<int> {
@@ -157,129 +160,160 @@ auto rng = [] {
     std::seed_seq seq(std::begin(seed_data), std::end(seed_data));
     return std::mt19937{seq};
 };
-} // namespace
 
 std::mutex print_mut{};
+auto print(auto... args) {
+    std::lock_guard l{print_mut};
+    ((std::cout << std::this_thread::get_id() << ": ") << ... << args)
+        << std::endl;
+}
+} // namespace
 
 TEST_CASE("jeff2", "[jeff]") {
     using namespace async;
 
     msg_queue qa{};
     msg_queue qb{};
+    std::atomic<bool> a_listening{};
+    std::atomic<bool> b_listening{};
 
-    // worker thread A: simulating an ISR on A receiving messages from B
-    std::jthread worker_a{[&](std::stop_token st) {
-        auto r = rng();
-        while (not st.stop_requested()) {
-            if (auto x = qa.try_pop(); x) {
-                {
-                    std::lock_guard l{print_mut};
-                    std::cout << "received " << *x << " (b to a)" << std::endl;
-                }
-                if (std::uniform_int_distribution{0, 255}(r) < 128) {
+    std::atomic<bool> interrupt_a{};
+    std::atomic<bool> interrupt_b{};
+
+    auto r_a = rng();
+    auto service_a = [&] {
+        if (auto x = qa.try_pop(); x) {
+            print("a received ", *x, " from b");
+            if (a_listening.exchange(false)) {
+                if (std::uniform_int_distribution{0, 255}(r_a) < 128) {
                     run_triggers<"a1">(*x);
                 } else {
                     run_triggers<"a2">(*x);
                 }
             }
         }
+    };
+
+    // worker thread A: simulating an ISR on A receiving messages from B
+    std::jthread worker_a{[&](std::stop_token st) {
+        while (not st.stop_requested()) {
+            if (interrupt_a.exchange(false)) {
+                service_a();
+            }
+        }
     }};
 
-    // worker thread B: simulating an ISR on B receiving messages from A
-    std::jthread worker_b{[&](std::stop_token st) {
-        auto r = rng();
-        while (not st.stop_requested()) {
-            if (auto x = qb.try_pop(); x) {
-                {
-                    std::lock_guard l{print_mut};
-                    std::cout << "received " << *x << " (a to b)" << std::endl;
-                }
-                if (std::uniform_int_distribution{0, 255}(r) < 128) {
+    auto r_b = rng();
+    auto service_b = [&] {
+        if (auto x = qb.try_pop(); x) {
+            print("b received ", *x, " from a");
+            if (b_listening.exchange(false)) {
+                if (std::uniform_int_distribution{0, 255}(r_b) < 128) {
                     run_triggers<"b1">(*x);
                 } else {
                     run_triggers<"b2">(*x);
                 }
             }
         }
+    };
+
+    // worker thread B: simulating an ISR on B receiving messages from A
+    std::jthread worker_b{[&](std::stop_token st) {
+        while (not st.stop_requested()) {
+            if (interrupt_b.exchange(false)) {
+                service_b();
+            }
+        }
     }};
 
     auto send_to_a = [&](int x) {
-        {
-            std::lock_guard l{print_mut};
-            std::cout << "sending " << x << " (b to a)" << std::endl;
-        }
+        print("b sending ", x, " to a");
         qa.push(x);
+        interrupt_a = true;
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(200ms);
     };
     auto send_to_b = [&](int x) {
-        {
-            std::lock_guard l{print_mut};
-            std::cout << "sending " << x << " (a to b)" << std::endl;
-        }
+        print("a sending ", x, " to b");
         qb.push(x);
+        interrupt_b = true;
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(200ms);
     };
 
     std::atomic<int> a_count{};
     std::atomic<int> b_count{};
-    constexpr auto max_sends = 1000;
+    constexpr auto max_sends = 2;
 
     // A's loop sender
     auto sndr_a = when_any(trigger_scheduler<"a1", int>{}.schedule(),
                            trigger_scheduler<"a2", int>{}.schedule()) |
+                  then([](auto x) {
+                      print("a processing ", x);
+                      // A takes a while to process the packet
+                      using namespace std::chrono_literals;
+                      std::this_thread::sleep_for(1s);
+                      return x;
+                  }) |
                   repeat_until([&](auto) { return a_count > max_sends; },
-                               [&](auto) { send_to_b(++a_count); });
+                               [&](auto) {
+                                   a_listening = true;
+                                   print("a ready to recv again");
+                                   //  before A sends to B, reprocess the queue
+                                   interrupt_a = true;
+                                   send_to_b(++a_count);
+                               });
 
     // B's loop sender
     auto sndr_b = when_any(trigger_scheduler<"b1", int>{}.schedule(),
                            trigger_scheduler<"b2", int>{}.schedule()) |
+                  then([](auto x) {
+                      print("b processing ", x);
+                      return x;
+                  }) |
                   repeat_until([&](auto) { return b_count > max_sends; },
-                               [&](auto) { send_to_a(++b_count); });
+                               [&](auto) {
+                                   b_listening = true;
+                                   print("b ready to recv again");
+                                   interrupt_b = true;
+                                   send_to_a(++b_count);
+                               });
 
     // to synchronize the first send only when both sides are ready
-    auto start_ready = [] {
-        std::lock_guard l{print_mut};
-        std::cout << "ready for first send" << std::endl;
-    };
+    auto start_ready = [] { print("ready for first send"); };
     std::barrier start_sync_point(2, start_ready);
 
     std::jthread a{[&] {
-        {
-            std::lock_guard l{print_mut};
-            std::cout << "thread a running" << std::endl;
-        }
+        print("thread a running");
         [[maybe_unused]] auto result =
             just([&] {
                 // this self-trigger starts sndr_a
                 run_triggers<"start a">();
+                b_listening = true;
                 start_sync_point.arrive_and_wait();
-                // first send: here we know both sides are ready to receive
+                // first send: here we know both sides
+                // are ready to receive
                 send_to_b(a_count);
             }) |
             incite_on(trigger_scheduler<"start a">{}) | seq(sndr_a) |
             sync_wait();
-        {
-            std::lock_guard l{print_mut};
-            std::cout << "thread a done" << std::endl;
-        }
+        print("thread a done");
     }};
 
     std::jthread b{[&] {
-        {
-            std::lock_guard l{print_mut};
-            std::cout << "thread b running" << std::endl;
-        }
+        print("thread b running");
         [[maybe_unused]] auto result =
             just([&] {
                 // this self-trigger starts sndr_b
                 run_triggers<"start b">();
+                a_listening = true;
                 start_sync_point.arrive_and_wait();
-                // first send: here we know both sides are ready to receive
+                // first send: here we know both sides
+                // are ready to receive
                 send_to_a(b_count);
             }) |
             incite_on(trigger_scheduler<"start b">{}) | seq(sndr_b) |
             sync_wait();
-        {
-            std::lock_guard l{print_mut};
-            std::cout << "thread b done" << std::endl;
-        }
+        print("thread b done");
     }};
 }
